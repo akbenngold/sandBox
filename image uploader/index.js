b75f8cdc9548a1180b4d417b9ac4b749dc542e62
Benjamin Akogwu <72498960+akbenngold@users.noreply.github.com>
2024-02-06 22:58:55 +0100

    Update index.js

--- image uploader/index.js
+++ image uploader/index.js
@@ -1,34 +1,32 @@
-// function uploadImage() {
-//   const fileInput = document.getElementById("uploadInput");
+function uploadImage() {
+  const fileInput = document.getElementById("uploadInput");
 
-//   const imageContainer = document.getElementById("imageContainer");
+  const imageContainer = document.getElementById("imageContainer");
 
-//   //   check if file is selected
-//   if (fileInput.files.length > 0) {
-//     const file = fileInput.files[0];
-//     // check if file is an image
-//     if (file.type.match("image.*")) {
-//       const reader = new FileReader();
+  //   check if file is selected
+ if (fileInput.files.length > 0) {
+  const file = fileInput.files[0];
+ // check if file is an image
+   if (file.type.match("image.*")) {
+  const reader = new FileReader();
 
-//       reader.onload = function (e) {
-//         const img = new Image();
+  reader.onload = function (e) {
+     const img = new Image();
 
-//         img.src = e.target.result;
+   img.src = e.target.result;
 
-//         img.width = 200;
+      img.width = 200;
 
-//         imageContainer.innerHTML = "";
+       imageContainer.innerHTML = "";
 
-//         imageContainer.appendChild(img);
-//       };
-//       reader.readAsDataURL(file);
-//     } else {
-//       alert("please select an image");
-//     }
-//   } else {
-//     alert("please select a file");
-//   }
-// }
+    imageContainer.appendChild(img);
+      };
+    reader.readAsDataURL(file);
+    } else {
+     alert("please select an image");
+   }
+  } else {
+alert("please select a file");
+   }
+}
 
-const imageContainer = document.getElementById("imageContainer");
-console.log(imageContainer);
